36e75c32477f396fb99352e5801e5a7b3c8c666b
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-08-20 09:35:13 +0200

    Mobile and tablet version for navbar

--- leo/src/components/navbar/navbar.jsx
+++ leo/src/components/navbar/navbar.jsx
@@ -1,9 +1,20 @@
+import React, { useState } from 'react';
 import styles from './navbar.module.scss'
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
-        <nav className={styles.navbar}>
+        <nav className={`${styles.navbar} ${isOpen ? styles.open : ''}`}>
+            <div className={styles.menuIcon} onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
             <ul className={styles.navLinks}>
                 <li><a href="#hjem">Hjem</a></li>
                 <li className={styles.separator}>|</li>
@@ -17,6 +28,7 @@ export const Navbar = () => {
                 <span className={styles.leo}>Leo</span>
                 <span className={styles.lov}>-Lov</span>
             </div>
+            
         </nav>
     );
 };
